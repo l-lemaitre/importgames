a9a65f208650bfbe943302344684cf7bce9abe71
l-lemaitre <77299640+l-lemaitre@users.noreply.github.com>
2021-08-11 18:11:00 +0200

    ImportGames v1.0 Front-office
    Tout les fichiers présents sur cette page ainsi que les dossiers images et videos sont téléchargeables dans la section releases.

---
+++ application/bdd_co_fonctions.php
@@ -0,0 +1,4 @@
+<?php
+    // Établi une connexion avec la base de données "importgames" pour les fonctions PHP (voir fichier categorie.php ligne 64 pour exemple d'utilisation)
+    $pdo = new PDO("mysql:host=localhost;dbname=importgames;charset=UTF8", "UTILISATEUR", "MOT DE PASSE");
+?>
